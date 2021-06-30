6401a757d988bc7839ed9d16aaf9a7a94e6f2199
T Yoshizawa <t-yoshi@users.noreply.github.com>
2021-06-30 13:39:52 +0900

    非推奨: サイドローディングインストールしたユーザーに警告

--- app/src/main/java/org/peercast/pecaplay/PecaPlayApplication.kt
+++ app/src/main/java/org/peercast/pecaplay/PecaPlayApplication.kt
@@ -2,7 +2,6 @@ package org.peercast.pecaplay
 
 import android.app.Application
 import android.util.Log
-import com.google.android.play.core.missingsplits.MissingSplitsManagerFactory
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
@@ -21,7 +20,7 @@ import timber.log.Timber
 import java.util.*
 
 
-val appModule = module {
+private val appModule = module {
     single { AppRoomDatabase.createInstance(get(), "pecaplay-5") }
     single<AppPreferences> { DefaultAppPreferences(get()) }
     single { LoadingEventFlow() }
@@ -33,11 +32,6 @@ class PecaPlayApplication : Application() {
     private val appPrefs: AppPreferences by inject()
 
     override fun onCreate() {
-        //不完全なapkをサイドローディングインストールしたユーザーに警告する
-        if (MissingSplitsManagerFactory.create(this).disableAppIfMissingRequiredSplits()) {
-            return
-        }
-
         super.onCreate()
 
         Timber.plant(ReleaseTree())
@@ -52,16 +46,16 @@ class PecaPlayApplication : Application() {
         AppTheme.initNightMode(this, appPrefs.isNightMode)
     }
 
+    private class ReleaseTree : Timber.DebugTree() {
+        override fun isLoggable(tag: String?, priority: Int): Boolean {
+            return priority >= Log.INFO || BuildConfig.DEBUG
+        }
+
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            super.log(priority, tag, message, t)
+            if (t != null)
+                FirebaseCrashlytics.getInstance().recordException(t)
+        }
+    }
 }
 
-private class ReleaseTree : Timber.DebugTree() {
-    override fun isLoggable(tag: String?, priority: Int): Boolean {
-        return priority >= Log.INFO || BuildConfig.DEBUG
-    }
-
-    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        super.log(priority, tag, message, t)
-        if (t != null)
-            FirebaseCrashlytics.getInstance().recordException(t)
-    }
-}
