300ae349907407df6bffa481bcdddfdb7cc2d215
T Yoshizawa <t-yoshi@users.noreply.github.com>
2021-08-11 03:31:39 +0900

    PostMessageDialogFragment

--- pecaviewer/src/main/java/org/peercast/pecaviewer/chat/PostMessageDialogFragment.kt
+++ pecaviewer/src/main/java/org/peercast/pecaviewer/chat/PostMessageDialogFragment.kt
@@ -5,19 +5,18 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
-import android.widget.EditText
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.peercast.pecaviewer.R
 import org.peercast.pecaviewer.chat.net.IBoardThreadPoster
 import org.peercast.pecaviewer.chat.net.PostMessage
+import org.peercast.pecaviewer.databinding.PostMessageDialogFragmentBinding
 
 class PostMessageDialogFragment : BottomSheetDialogFragment(),
     DialogInterface.OnShowListener {
     private val chatViewModel by sharedViewModel<ChatViewModel>()
+    private lateinit var binding: PostMessageDialogFragmentBinding
     private lateinit var poster: IBoardThreadPoster
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,47 +25,44 @@ class PostMessageDialogFragment : BottomSheetDialogFragment(),
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        binding = PostMessageDialogFragmentBinding.inflate(layoutInflater, null, false)
         return super.onCreateDialog(savedInstanceState).also { d ->
-            d.setContentView(R.layout.fragment_post_message_dialog)
+            d.setContentView(binding.root)
             d.setOnShowListener(this)
         }
     }
 
     override fun onShow(dialog: DialogInterface) {
         val u = poster.info.url
+        dialog as BottomSheetDialog
 
-        with(dialog as BottomSheetDialog) {
-            val vEdit = checkNotNull(findViewById<EditText>(R.id.vEdit))
-            val vSend = checkNotNull(findViewById<Button>(R.id.vSend))
+        chatViewModel.messageDraft[u]?.let(binding.vEdit::setText)
+        val sendClickListener = View.OnClickListener {
+            binding.vEdit.isEnabled = false
+            binding.vSend.isEnabled = false
+            chatViewModel.presenter.postMessage(
+                poster,
+                PostMessage("", "sage", binding.vEdit.text.toString())
+            )
+            chatViewModel.messageDraft.remove(u)
+            dismiss()
+        }
 
-            chatViewModel.messageDraft[u]?.let(vEdit::setText)
-            val sendClickListener = View.OnClickListener {
-                vEdit.isEnabled = false
-                vSend.isEnabled = false
-                chatViewModel.presenter.postMessage(
-                    poster,
-                    PostMessage("", "sage", vEdit.text.toString())
-                )
-                chatViewModel.messageDraft.remove(u)
-                dismiss()
+        binding.vSend.setOnClickListener(sendClickListener)
+        binding.vEdit.setOnEditorActionListener { v, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEND && binding.vSend.isEnabled) {
+                sendClickListener.onClick(v)
+                true
+            } else {
+                false
             }
+        }
+        binding.vEdit.hint = "${poster.info.title} (${poster.info.numMessages})"
+        binding.vEdit.requestFocus()
 
-            vSend.setOnClickListener(sendClickListener)
-            vEdit.setOnEditorActionListener { v, actionId, _ ->
-                if (actionId == EditorInfo.IME_ACTION_SEND && vSend.isEnabled) {
-                    sendClickListener.onClick(v)
-                    true
-                } else {
-                    false
-                }
-            }
-            vEdit.hint = "${poster.info.title} (${poster.info.numMessages})"
-            vEdit.requestFocus()
-
-            vEdit.doOnTextChanged { text, _, _, _ ->
-                chatViewModel.messageDraft[u] = text?.toString() ?: ""
-                vSend.isEnabled = text?.isNotEmpty() == true
-            }
+        binding.vEdit.doOnTextChanged { text, _, _, _ ->
+            chatViewModel.messageDraft[u] = text?.toString() ?: ""
+            binding.vSend.isEnabled = text?.isNotEmpty() == true
         }
     }
 
