9f7ea0ef0597637977c4b500cb2c198a7130c8ea
T Yoshizawa <t-yoshi@users.noreply.github.com>
2021-08-10 17:30:38 +0900

    PipPlayerFragmentBinding

--- pecaviewer/src/main/java/org/peercast/pecaviewer/PipPlayerFragment.kt
+++ pecaviewer/src/main/java/org/peercast/pecaviewer/PipPlayerFragment.kt
@@ -10,16 +10,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.google.android.exoplayer2.ui.PlayerView
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import org.peercast.pecaplay.core.app.PecaViewerIntent
 import org.peercast.pecaplay.core.app.Yp4gChannel
+import org.peercast.pecaviewer.databinding.PipPlayerFragmentBinding
 import org.peercast.pecaviewer.service.PlayerService
 import org.peercast.pecaviewer.service.bindPlayerService
 import timber.log.Timber
@@ -27,7 +26,7 @@ import timber.log.Timber
 class PipPlayerFragment : Fragment(), ServiceConnection {
 
     private var service: PlayerService? = null
-    private var vPlayer: PlayerView? = null
+    private var binding: PipPlayerFragmentBinding? = null
     private lateinit var channel: Yp4gChannel
     private val isVisibleTitleBar = MutableStateFlow(true)
 
@@ -48,20 +47,21 @@ class PipPlayerFragment : Fragment(), ServiceConnection {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        return inflater.inflate(R.layout.pip_player_fragment, container, false)
+        return PipPlayerFragmentBinding.inflate(inflater, container, false).also {
+            binding = it
+            it.title = channel.name
+            it.isVisibleTitleBar = isVisibleTitleBar
+            it.lifecycleOwner = viewLifecycleOwner
+        }.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        vPlayer = view.findViewById(R.id.vPlayer)
-        val vTitle = view.findViewById<TextView>(R.id.vTitle)
-        val vTitleBar = view.findViewById<ViewGroup>(R.id.vTitleBar)
-        vTitle.text = channel.name
 
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             isVisibleTitleBar.filter { it }.collect {
                 delay(6_000)
-                vTitleBar.startAnimation(AlphaAnimation(1f, 0f).apply {
+                binding?.vTitleBar?.startAnimation(AlphaAnimation(1f, 0f).apply {
                     duration = 200
                     fillAfter = true
                 })
@@ -79,14 +79,14 @@ class PipPlayerFragment : Fragment(), ServiceConnection {
     override fun onServiceConnected(name: ComponentName?, binder: IBinder) {
         Timber.d("onServiceConnected")
         service = (binder as PlayerService.Binder).service.also {
-            vPlayer?.let(it::attachPlayerView)
+            binding?.vPlayer?.let(it::attachPlayerView)
         }
     }
 
     override fun onServiceDisconnected(name: ComponentName?) {
         Timber.d("onServiceDisconnected")
         service = null
-        vPlayer?.player = null
+        binding?.vPlayer?.player = null
     }
 
     override fun onStart() {
@@ -106,7 +106,7 @@ class PipPlayerFragment : Fragment(), ServiceConnection {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        vPlayer = null
+        binding = null
     }
 
 }
